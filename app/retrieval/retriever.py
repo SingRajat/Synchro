6002a91dce873c3738a078de9d9c12ce073810ca
Rajat Singh <rajatkvongc@gmail.com>
2026-04-29 00:40:50 +0530

    Added Hybrid search Vector+BM25

--- app/retrieval/retriever.py
+++ app/retrieval/retriever.py
@@ -1,14 +1,21 @@
 from langchain_core.runnables import RunnableLambda
-def Retriever(db):
-    retriever=db.as_retriever(
+from langchain_community.retrievers import EnsembleRetriever
+def Retriever(db,bm25_retriever):
+    dense_retriever=db.as_retriever(
         search_kwargs={"k": 3}
     )
+
+    ensemble=EnsembleRetriever(
+        retrievers=[dense_retriever,bm25_retriever],
+        weight=[0.5,0.5]
+    )
     def get_unique_docs(queries):
-        for q in queries:
-            docs=(retriever.invoke(q))
         unique_docs={}
-        for doc in docs:
-            if doc.page_content not in unique_docs:
-                unique_docs[doc.page_content]=doc
+        for q in queries:
+            docs=(ensemble.invoke(q))
+            for doc in docs:
+                if doc.page_content not in unique_docs:
+                    unique_docs[doc.page_content]=doc
         return list(unique_docs.values())  
-    return RunnableLambda(get_unique_docs())
+    return RunnableLambda(get_unique_docs)
+    
